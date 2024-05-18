1f7aa86de733cd921f617e4cd1eabb9785ccda39
Christopher Trent <ChristopherJTrent@outlook.com>
2024-05-18 11:29:01 -0700

    Scaffold in Gamepad Editor

--- src/Components/Launcher.tsx
+++ src/Components/Launcher.tsx
@@ -3,21 +3,30 @@ import AddonListing from "./Widgets/AddonListing"
 import DummyAddons from "../lib/data/Dummy"
 import AppLayout from "./Layouts/App"
 import SettingsEditor from "./Widgets/SettingsEditor"
+import GamepadEditor from "./Widgets/GamepadEditor"
 
 export default function Launcher() {
   return (
   <AppLayout>
     <Tabs width='90%' colorScheme="orange">
       <TabList>
+		    <Tab>Profiles</Tab>
         <Tab>Addons</Tab>
         <Tab>Settings</Tab>
+		    <Tab>Controller Settings</Tab>
       </TabList>
       <TabPanels>
+        <TabPanel>
+          Coming Soon!
+        </TabPanel>
         <TabPanel>
           <AddonListing addons={DummyAddons}/>
         </TabPanel>
         <TabPanel>
           <SettingsEditor />
+        </TabPanel>
+        <TabPanel>
+          <GamepadEditor />
         </TabPanel>
       </TabPanels>
     </Tabs>
--- src/Components/Widgets/GamepadEditor.tsx
+++ src/Components/Widgets/GamepadEditor.tsx
@@ -1,8 +1,24 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { EditIcon } from "@chakra-ui/icons"
-import { Button } from "@chakra-ui/react"
+import { Button, Flex, Switch } from "@chakra-ui/react"
+import { useState } from "react"
 
 export default function GamepadEditor() {
+  const [gamepadSettings, setGamepadSettings] = useState([0,0,0,0,0,0])
+  function setFlag(index:number, value:(1|0)) {
+    const temp = gamepadSettings
+    temp[index] = value
+    setGamepadSettings(temp)
+  }
   return <>
-    <Button>Gamepad Settings <EditIcon /></Button>
+  <Button>Gamepad Settings <EditIcon /></Button>
+	<Flex direction='column'>
+		<label>Enable Gamepad
+		  <Switch id="enableGamepad" 
+        isChecked={gamepadSettings[0] === 0}
+        onChange={(e) =>  setFlag(0, e.target.checked ? 1 : 0)}/>
+    </label>
+    
+	</Flex>
   </>
 }
