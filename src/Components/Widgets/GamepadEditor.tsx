363eb8899672d305ea27f6257a251d0cf9df3ae2
Christopher Trent <ChristopherJTrent@outlook.com>
2024-05-18 14:49:03 -0700

    move toggles to iterative style

--- src/Components/Widgets/GamepadEditor.tsx
+++ src/Components/Widgets/GamepadEditor.tsx
@@ -1,31 +1,59 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { EditIcon } from "@chakra-ui/icons"
-import { Button, Flex, Switch } from "@chakra-ui/react"
+import { Flex, Switch } from "@chakra-ui/react"
 import { useAppDispatch, useAppSelector } from "../../lib/store/store"
 import { setToggle } from "../../lib/store/GamepadSettings"
+
+type UIDef = {
+  key: string
+  label: string
+
+}
+
+const toggleUIDefs:UIDef[] = [
+  {
+    key: 'enabled',
+    label:'Enable Gamepad'
+  },
+  {
+    key: 'rumble',
+    label: 'Enable Rumble'
+  },
+  {
+    key:'sliders',
+    label:'Enable Sliders'
+  },
+  {
+    key: 'hats',
+    label: 'Enable Hat Switches'
+  },
+  {
+    key: 'inactive',
+    label:'Enable Input when Inactive'
+  },
+  {
+    key: 'xinput',
+    label: 'XInput (Xbox)'
+  }
+]
+
 
 export default function GamepadEditor() {
   const flags = useAppSelector((state) => state.gamepad.toggles)
   const dispatch = useAppDispatch()
-  return <>
-  <Button>Gamepad Settings <EditIcon /></Button>
-	<Flex direction='row' flexWrap='wrap' justifyContent='space-around'>
-		<label>Enable Gamepad
-		  <Switch id="enableGamepad" 
-        isChecked={flags.enabled}
-        onChange={() => dispatch(setToggle({
-          name:'enabled',
-          value: !flags.enabled
-        }))}/>
-    </label>
-    <label> Enable Rumble
-      <Switch 
-        isChecked={flags.rumble}
-        onChange={() => dispatch(setToggle({
-          name:'rumble',
-          value: !flags.rumble
-        }))} />
-    </label>
+  return <Flex direction='row' flexWrap='wrap' justifyContent='space-around'>
+    {
+      toggleUIDefs.map((v) => (
+        <label style={{width: '45%'}}>
+          <Switch
+            isChecked={flags[v.key as keyof typeof flags]}
+            onChange={() => dispatch(setToggle({
+              name: v.key,
+              value: !(flags[v.key as keyof typeof flags])
+            }))} marginRight='8px' />
+            {v.label}
+        </label>
+      ))
+    }
 	</Flex>
-  </>
+
 }
