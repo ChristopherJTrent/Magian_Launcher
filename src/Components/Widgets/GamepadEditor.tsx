88e72988e4b6c5ac3c47846866fe8ebafa37c6a8
Christopher Trent <ChristopherJTrent@outlook.com>
2024-05-18 13:19:39 -0700

    start work on gamepad editor

--- src/Components/Widgets/GamepadEditor.tsx
+++ src/Components/Widgets/GamepadEditor.tsx
@@ -1,24 +1,29 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { EditIcon } from "@chakra-ui/icons"
 import { Button, Flex, Switch } from "@chakra-ui/react"
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function GamepadEditor() {
   const [gamepadSettings, setGamepadSettings] = useState([0,0,0,0,0,0])
-  function setFlag(index:number, value:(1|0)) {
+  function setFlag(index:number, evt: React.ChangeEvent<HTMLInputElement>) {
     const temp = gamepadSettings
-    temp[index] = value
+    temp[index] = gamepadSettings[index] === 0 ? 1 : 0
+    console.log(temp)
     setGamepadSettings(temp)
   }
   return <>
   <Button>Gamepad Settings <EditIcon /></Button>
-	<Flex direction='column'>
+	<Flex direction='row' flexWrap='wrap' justifyContent='space-around'>
 		<label>Enable Gamepad
 		  <Switch id="enableGamepad" 
-        isChecked={gamepadSettings[0] === 0}
-        onChange={(e) =>  setFlag(0, e.target.checked ? 1 : 0)}/>
+        isChecked={gamepadSettings[0] == 1}
+        onChange={(e) =>  setFlag(0, e)}/>
     </label>
-    
+    <label> Enable Rumble
+      <Switch 
+        isChecked={gamepadSettings[1] === 1}
+        onChange={(e) => setFlag(1, e)} />
+    </label>
 	</Flex>
   </>
 }
