4f9d61ff1f282b5fb057d02f506680f27eb25cb5
Christopher Trent <ChristopherJTrent@outlook.com>
2024-05-28 16:22:47 -0700

    add asstd methods

--- src/lib/util/Installation/paths.ts
+++ src/lib/util/Installation/paths.ts
@@ -3,6 +3,10 @@ import { app } from "electron"
 
 export const INSTALL_LOCATION = `${app.getPath('home')}\\Magian Launcher`
 export const ASHITA_LOCATION = `${INSTALL_LOCATION}\\Ashita`
+export const PROFILE_LOCATION = `${INSTALL_LOCATION}\\Profiles`
+export const CONFIGURATION_LOCATION = `${ASHITA_LOCATION}\\config\\boot\\managed`
+export const SCRIPT_LOCATION = `${ASHITA_LOCATION}\\scripts`
+export const MANAGED_SCRIPT_LOCATION = `${SCRIPT_LOCATION}\\managed`
 
 export function hasGit():boolean {
   const bat = spawnSync('powershell.exe',[
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,12 +1,14 @@
 // Disable no-unused-vars, broken for spread args
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer} from 'electron'
+import Profile from '../lib/data/Profile'
 
 export type Channels = 'ipc-example';
 
 const electronHandler = {
   ipcRenderer: {
-    updateAshita: () => ipcRenderer.invoke('ashita:update') as Promise<void>
+    updateAshita: () => ipcRenderer.invoke('ashita:update') as Promise<void>,
+    loadProfiles: () => ipcRenderer.invoke('magian:loadProfiles') as Promise<Profile[]>
   },
 }
 
