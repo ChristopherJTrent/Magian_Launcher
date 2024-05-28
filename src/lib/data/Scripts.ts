72486f6bf8773712538c738cd6b2e339063a5e6d
Christopher Trent <ChristopherJTrent@outlook.com>
2024-05-28 16:22:06 -0700

    Create scaffold script

---
+++ src/lib/data/Scripts.ts
@@ -0,0 +1,21 @@
+export function generateManagedScript(name:string, addons:string[] | undefined):string {
+  return `
+  ;---------------------------------------------------------------------------;
+  ; This script is managed by Magian Launcher. It will be overwritten         ;
+  ; when you enable or disable any addons or plugins.                         ;
+  ; Please edit the profile hook scripts provided in scripts/${name}.         ;
+  ;---------------------------------------------------------------------------;
+  /exec ${name}/beforePluginLoad
+  /load thirdparty
+  /load screenshot
+
+  /exec ${name}/beforeAddonLoad
+  ${addons?.map((v) => `/addon load ${v}`)?.join('\n') ?? '\n'}
+  /exec ${name}/keybinds
+  /wait 3
+  /exec ${name}/addonInit`
+}
+
+export function generateHookScript() {
+
+}
