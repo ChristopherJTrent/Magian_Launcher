3cec143c06014b6effccfa50324f5b1324412a04
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-03 10:41:40 -0700

    impl profile saving

--- src/lib/data/Profile.ts
+++ src/lib/data/Profile.ts
@@ -1,15 +1,10 @@
-import { receiveSettings } from "../store/AshitaSettingsReducer"
-import { store } from "../store/store"
-import retail from "../util/Config/DefaultConfiguration"
-
 export default class Profile {
   public name:string
 
-  constructor(name: string) {
-    this.name = name
-  }
+  public enabledAddons: string[]
 
-  public load() {
-    store.dispatch(receiveSettings(retail(this.name)))
+  constructor(name: string, enabledAddons:string[] = []) {
+    this.name = name
+    this.enabledAddons = enabledAddons
   }
 }
--- src/lib/util/IO/ProfileLoader.ts
+++ src/lib/util/IO/ProfileLoader.ts
@@ -1,8 +1,11 @@
-import { readFile, readdir } from "fs/promises"
-import { PROFILE_LOCATION } from "../Installation/paths"
+import { mkdir, readFile, readdir, writeFile } from "fs/promises"
+import { CONFIGURATION_LOCATION, MANAGED_SCRIPT_LOCATION, PROFILE_LOCATION } from "../Installation/paths"
 import Profile from "../../data/Profile"
+import { AshitaSettings } from "../../store/AshitaSettingsReducer"
+import { existsSync } from "fs"
+import { dumpINI } from "../Config/INIHelper"
 
-export default async function loadProfiles():Promise<Profile[]> {
+export async function loadProfiles():Promise<Profile[]> {
   const profiles:Profile[] = []
   const profileLocations = (await readdir(PROFILE_LOCATION, {withFileTypes: true}))
     .filter(entry => entry.isDirectory())
@@ -13,4 +16,19 @@ export default async function loadProfiles():Promise<Profile[]> {
     )
   )
   return profiles
+}
+
+export async function saveProfile(input:Profile, settings:AshitaSettings):Promise<void> {
+  const PROFILE_DIR = `${PROFILE_LOCATION}\\${input.name}`
+  if (!existsSync(PROFILE_DIR)) {
+    await mkdir(PROFILE_DIR)
+  }
+  try{
+    await writeFile(`${PROFILE_DIR}\\profile.json`, JSON.stringify(input), { flag: 'w+'})
+    await writeFile(
+      `${CONFIGURATION_LOCATION}\\${input.name}.ini`, 
+      dumpINI(settings), 
+      {flag: 'w+'}
+    )
+  }
 }
