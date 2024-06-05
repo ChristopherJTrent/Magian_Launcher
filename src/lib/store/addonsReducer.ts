2e172a1b98fa79539bab3366960e114d077ee6cc
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-04 22:15:40 -0700

    implement addons in redux

--- src/lib/store/addonsReducer.ts
+++ src/lib/store/addonsReducer.ts
@@ -1,16 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 
-export type addonsMapping = {[id: number]: string}
+export type addonsMapping = string[]
 
 // this will change in the future when repos are fully implemented.
-const initialState:addonsMapping = {}
+const initialState:addonsMapping = []
 
 export const addonsSlice = createSlice({
   name: 'addons',
   initialState,
   reducers: {
+    receiveAddons:  (state: addonsMapping, action: PayloadAction<string[]>) => {
+      action.payload.forEach((v, i) => {state[i] = v})
+    }
   }
 })
+
+export const {receiveAddons} = addonsSlice.actions
 
 export default addonsSlice.reducer
 
