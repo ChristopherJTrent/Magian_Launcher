47393cd41eaa63feab19ae1c9cd2a5ce5ce5863b
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-05 17:26:07 -0700

    refactor profile loader

--- src/lib/util/IO/ProfileLoader.ts
+++ src/lib/util/IO/ProfileLoader.ts
@@ -1,27 +1,27 @@
 import { mkdir, readFile, readdir, writeFile } from "fs/promises"
 import { existsSync } from "fs"
-import { CONFIGURATION_LOCATION, PROFILE_LOCATION } from "../Installation/paths"
+import { CONFIGURATION_LOCATION, MANAGED_SCRIPT_LOCATION, PROFILE_LOCATION, SCRIPT_LOCATION } from "../Installation/paths"
 import Profile from "../../data/Profile"
 import { AshitaSettings } from "../../store/AshitaSettingsReducer"
 import { dumpINI } from "../Config/INIHelper"
+import { generateManagedScript } from "../../data/Scripts"
 
 export async function loadProfiles():Promise<Profile[]> {
-  const profiles:Profile[] = []
-  const profileLocations = (await readdir(PROFILE_LOCATION, {withFileTypes: true}))
-    .filter(entry => entry.isDirectory())
-    .map(entry => `${entry.path}\\profile.json`)
-  profileLocations.forEach(
-    (v) => readFile(v).then(
-      contents => profiles.push(JSON.parse(contents.toString()) as Profile)
-    )
+  return Promise.all((await readdir(PROFILE_LOCATION, {withFileTypes: true}))
+    .filter(entry => {
+      return entry.isDirectory()})
+    .map(entry => `${PROFILE_LOCATION}\\${entry.name}\\profile.json`)
+    .map(async path => JSON.parse((await readFile(path)).toString()) as Profile)
   )
-  return profiles
 }
 
 export async function saveProfile(input:Profile, settings:AshitaSettings):Promise<void> {
   const PROFILE_DIR = `${PROFILE_LOCATION}\\${input.name}`
   if (!existsSync(PROFILE_DIR)) {
     await mkdir(PROFILE_DIR)
+  }
+  if (!existsSync(CONFIGURATION_LOCATION)) {
+    await mkdir(CONFIGURATION_LOCATION)
   }
   try{
     await writeFile(`${PROFILE_DIR}\\profile.json`, JSON.stringify(input), { flag: 'w+'})
@@ -30,7 +30,32 @@ export async function saveProfile(input:Profile, settings:AshitaSettings):Promis
       dumpINI(settings), 
       {flag: 'w+'}
     )
-  } catch(e) {
-    console.log(e)
+    await writeFile(
+      `${MANAGED_SCRIPT_LOCATION}\\${input.name}.txt`,
+      generateManagedScript(input.name, input.enabledAddons),
+      {flag: 'w+'}
+    )
+  } catch(e) { /* empty */ }
+}
+
+export async function initializeProfile(name: string):Promise<void> {
+  if (!existsSync(PROFILE_LOCATION)) {
+    await mkdir(PROFILE_LOCATION)
+  }  
+  if (!existsSync(`${PROFILE_LOCATION}\\${name}`)) {
+    await mkdir(`${PROFILE_LOCATION}\\${name}`)
   }
+  if (!existsSync(`${SCRIPT_LOCATION}\\${name}`)) {
+    await mkdir(`${SCRIPT_LOCATION}\\${name}`)
+  }
+  ['beforePluginLoad','beforeAddonLoad','keybinds','addonInit'].forEach(async (v) => {
+    const hookPath = `${SCRIPT_LOCATION}\\${name}\\${v}.txt`
+    if(!existsSync(hookPath)) {
+      try {
+        await writeFile(hookPath, '', {flag: 'w+'})
+      } catch {
+        /* empty */
+      }
+    }
+  })
 }
