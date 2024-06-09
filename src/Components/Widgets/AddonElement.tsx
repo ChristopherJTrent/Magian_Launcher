4596397048c0ad258138a2d9e752c2c66c0bad37
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-09 09:31:46 -0700

    add switches to addon listing

--- src/Components/Widgets/AddonElement.tsx
+++ src/Components/Widgets/AddonElement.tsx
@@ -1,26 +1,46 @@
-import { AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Switch, Text } from "@chakra-ui/react"
-import { useState } from "react"
-import Addon from "../../lib/data/Addon"
+import { useSelector } from "react-redux"
+import { Flex, Switch } from "@chakra-ui/react"
+import { addonEnabled, setAddonDisabled, setAddonEnabled } from "../../lib/store/ProfileReducer"
+import { useAppDispatch } from "../../lib/store/store"
 
 export type AddonElementProps = {
-  addon: Addon
+  addon: string
 }
 
 export default function AddonElement({addon}:AddonElementProps) {
-  const [addonEnabled, setAddonEnabled] = useState(false)
-  return <AccordionItem >
-    <h2>
-      <AccordionButton _hover={{transform: '', opacity: 1}}>
-        <Box as="span" flex='1' textAlign='left'>
-          {addon.Name}
-        </Box>
-        <Switch isChecked={addonEnabled} onChange={() => setAddonEnabled(! addonEnabled)} colorScheme="orange"/>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel>
-      <Text fontSize='sm' color='GrayText'>{addon.Author} ({addon.AuthorContact})</Text>
-      <Text fontSize='lg' overflowWrap='break-word'>{addon.Description}</Text>
-    </AccordionPanel>
-  </AccordionItem>
+  // const [addonEnabled, setAddonEnabled] = useState(false)
+  // return <AccordionItem >
+  //  <h2>
+  //    <AccordionButton _hover={{transform: '', opacity: 1}}>
+  //      <Box as="span" flex='1' textAlign='left'>
+  //        {addon.Name}
+  //      </Box>
+  //      <Switch isChecked={addonEnabled} onChange={() => setAddonEnabled(! addonEnabled)} colorScheme="orange"/>
+  //      <AccordionIcon />
+  //    </AccordionButton>
+  //  </h2>
+  //  <AccordionPanel>
+  //    <Text fontSize='sm' color='GrayText'>{addon.Author} ({addon.AuthorContact})</Text>
+  //    <Text fontSize='lg' overflowWrap='break-word'>{addon.Description}</Text>
+  //  </AccordionPanel>
+  // </AccordionItem>
+  const enabled = useSelector(addonEnabled(addon))
+  const dispatch = useAppDispatch()
+
+  const toggleAddon = () => {
+    if (enabled) {
+      dispatch(setAddonDisabled(addon))
+    } else {
+      dispatch(setAddonEnabled(addon))
+    }
+  }
+  return <li>
+    <Flex justifyContent='space-between' direction='row'>
+      <h2>{addon}</h2>
+      <Switch
+        isChecked={enabled}
+        onChange={toggleAddon}
+      />
+    </Flex>
+  </li>
 }
--- src/Components/Widgets/AddonListing.tsx
+++ src/Components/Widgets/AddonListing.tsx
@@ -1,7 +1,9 @@
-import { Accordion, AccordionButton, AccordionItem } from "@chakra-ui/react"
 import React, { useEffect } from "react"
+import { Button, Flex } from "@chakra-ui/react"
+import { CheckIcon } from "@chakra-ui/icons"
 import { useAppDispatch, useAppSelector } from "../../lib/store/store"
 import { receiveAddons } from "../../lib/store/addonsReducer"
+import AddonElement from "./AddonElement"
 
 export default function AddonListing() {
   const addons = useAppSelector(state => state.addons)
@@ -12,8 +14,22 @@ export default function AddonListing() {
       return dispatch(receiveAddons(v))
     }).catch((_) => {})
   }, [dispatch])
-  console.log(addons)
-  return <ul>
-    {addons && addons.map((v, i) => <li key={i}>{v}</li>)}
-  </ul>
+  return <>
+    <Flex 
+        direction='column' 
+        overflowY='scroll' 
+        paddingRight='5px'
+        height='80vh'>
+      <ul>
+        {addons && addons.map((v) => <AddonElement addon={v}/>)}
+      </ul>
+    </Flex>
+    <Button marginTop='3px' 
+        width='100%' onClick={() => {}}>
+      <Flex>
+        <h1>Save</h1>
+        <CheckIcon marginLeft='10px'/>
+      </Flex>
+    </Button>
+  </>
 }
