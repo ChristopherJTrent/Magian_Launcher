828594a2c19c40a80614d43cf10000ed4d3afdbd
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-11 12:25:48 -0700

    Change main container to use icons

--- src/Components/Launcher.tsx
+++ src/Components/Launcher.tsx
@@ -1,10 +1,14 @@
-import { Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
+import { Flex, Icon, Tab, TabList, TabPanel, TabPanels, Tabs, Tooltip } from "@chakra-ui/react"
 import AddonListing from "./Widgets/AddonListing"
 import AppLayout from "./Layouts/App"
 import SettingsEditor from "./Widgets/SettingsEditor"
 import GamepadEditor from "./Widgets/GamepadEditor"
 import { useEffect } from "react"
 import ProfileListing from "./Widgets/ProfileListing"
+import { RiProfileLine } from 'react-icons/ri'
+import { SiCplusplus, SiLua } from 'react-icons/si'
+import { SettingsIcon } from "@chakra-ui/icons"
+import {PiGameController} from 'react-icons/pi'
 
 export default function Launcher() {
   useEffect(() => {
@@ -14,16 +18,50 @@ export default function Launcher() {
   <AppLayout>
     <Tabs width='90%' colorScheme="orange">
       <TabList>
-		    <Tab>Profiles</Tab>
-        <Tab>Addons</Tab>
-        <Tab>Settings</Tab>
-		    <Tab>Controller Settings</Tab>
+		    <Tab>
+          <Tooltip label='Profiles'>
+            <span>
+              <RiProfileLine />
+            </span>
+          </Tooltip>
+        </Tab>
+        <Tab>
+          <Tooltip label='Plugins'>
+            <span>
+              <SiCplusplus />
+            </span>
+          </Tooltip>
+        </Tab>
+        <Tab>
+          <Tooltip label='Addons'>
+            <span>
+              <SiLua/>
+            </span>
+          </Tooltip>
+        </Tab>
+        <Tab>
+          <Tooltip label='Ashita Settings'>
+            <span>
+              <SettingsIcon />
+            </span>
+          </Tooltip>
+        </Tab>
+		    <Tab>
+          <Tooltip label='Gamepad Settings'>
+            <span>
+              <PiGameController />
+            </span>
+          </Tooltip>
+        </Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
           <Flex overflowY='auto' flexDirection='column' height='80vh'>
             <ProfileListing />
           </Flex>
+        </TabPanel>
+        <TabPanel>
+          <span>Plugins: Coming Soon!</span>
         </TabPanel>
         <TabPanel>
           <AddonListing/>
