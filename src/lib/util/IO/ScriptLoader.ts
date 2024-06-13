03cf4048dd58f9aae12ca901262bc7bffd7e38a7
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-12 20:57:50 -0700

    add plugin support to scripts

--- src/lib/util/IO/ScriptLoader.ts
+++ src/lib/util/IO/ScriptLoader.ts
@@ -2,15 +2,16 @@ import { existsSync } from "fs"
 import { mkdir, writeFile } from "fs/promises"
 import { MANAGED_SCRIPT_LOCATION } from "../Installation/paths"
 import { generateManagedScript } from "../../data/Scripts"
+import Profile from "../../data/Profile"
 
-export default async function saveScript(name:string, addons:string[]) {
+export default async function saveScript(profile:Profile) {
   if(! existsSync(MANAGED_SCRIPT_LOCATION)) {
     await mkdir(MANAGED_SCRIPT_LOCATION)
   }
   try {
     await writeFile(
-      `${MANAGED_SCRIPT_LOCATION}\\${name}.txt`, 
-      generateManagedScript(name, addons),
+      `${MANAGED_SCRIPT_LOCATION}\\${profile.name}.txt`, 
+      generateManagedScript(profile.name, profile.enabledAddons, profile.enabledPlugins),
       {flag: 'w+'})
   } catch {
     /* empty */
