0e184dfe592bca4ca826386a344486c9207ab0a4
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-13 21:08:22 -0700

    Change app background to Ashita Grey

--- src/Components/Layouts/App.tsx
+++ src/Components/Layouts/App.tsx
@@ -5,13 +5,13 @@ export type AppLayoutProps = {}
 
 export default function AppLayout({children}:PropsWithChildren<AppLayoutProps>) {
   return (
-    <Box width='100vw' height='100vh'>
-      <Box width='100vw' height='30px' className='titlebar'>
+    <Box width='100vw' height='100vh' backgroundColor='#282C37'>
+      <Box width='100vw' height='30px' className='titlebar' margin='0px'>
         <Flex align='center'>
           <Text marginLeft='1em' paddingTop='2px'>Magian Launcher</Text>
         </Flex>
       </Box>
-      <Flex align='center' justify='center'>
+      <Flex align='center' justify='left'>
         {children}
       </Flex>
     </Box>
