c58456c1509d67042bd8b2995d5abddf9100be34
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-18 19:27:25 -0700

    add ashitasettings to profile

--- src/lib/data/Profile.ts
+++ src/lib/data/Profile.ts
@@ -1,13 +1,18 @@
+import AshitaSettings from "./AshitaSettings"
+
 export default class Profile {
   public name:string
 
   public enabledAddons: string[]
 
   public enabledPlugins: string[]
+  
+  public settings: AshitaSettings
 
   constructor(name: string, enabledAddons:string[] = [], enabledPlugins:string[] = []) {
     this.name = name
     this.enabledAddons = enabledAddons
     this.enabledPlugins = enabledPlugins
+    this.settings = new AshitaSettings(name)
   }
 }
