d49860994ee90e000b4907b18b6fdcea79299b53
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-19 20:32:57 -0700

    add private server flag

--- src/lib/data/Profile.ts
+++ src/lib/data/Profile.ts
@@ -8,11 +8,14 @@ export default class Profile {
   public enabledPlugins: string[]
   
   public settings: AshitaSettings
+  
+  public serverType: ('retail'|'private')
 
-  constructor(name: string, enabledAddons:string[] = [], enabledPlugins:string[] = []) {
+  constructor(name: string, enabledAddons:string[] = [], enabledPlugins:string[] = [], isPrivate: boolean = false) {
     this.name = name
     this.enabledAddons = enabledAddons
     this.enabledPlugins = enabledPlugins
     this.settings = new AshitaSettings(name)
+    this.serverType = isPrivate? 'private' : 'retail'
   }
 }
