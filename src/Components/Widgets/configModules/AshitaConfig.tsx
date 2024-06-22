36406f8557b14ba68a94e0a48a82bc20b706192e
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-21 18:55:54 -0700

    fix formatting on config editor

--- src/Components/Widgets/configModules/AshitaConfig.tsx
+++ src/Components/Widgets/configModules/AshitaConfig.tsx
@@ -18,10 +18,10 @@ export default function AshitaConfig() {
       </Text>
       <Divider marginBottom='5px'/>
       {
-        profile.serverType === 'private' 
+        profile.serverType === 'private'
         && <>
           <Flex direction='row' align='center'>
-            <Text as='h2' style={{textTransform: 'capitalize'}}>Boot File: </Text>
+            <Text as='h2' style={{textTransform: 'capitalize'}}>Boot&nbsp;File: </Text>
             <Editable defaultValue={profile.settings.bootFile}
               onSubmit={(next) => dispatch(setSettingsValue({field: 'bootFile', value: next}))}
               width='100%'>
@@ -33,7 +33,7 @@ export default function AshitaConfig() {
             </Editable>
           </Flex>
           <Flex direction='row' align='center'>
-            <Text as='h2' style={{textTransform: 'capitalize'}}>Boot Command: </Text>
+            <Text as='h2' style={{textTransform: 'capitalize'}}>Boot&nbsp;Command: </Text>
             <Editable defaultValue={profile.settings.bootCommand}
               onSubmit={(next) => dispatch(setSettingsValue({field: 'bootCommand', value: next}))}
               width='100%'>
@@ -45,7 +45,7 @@ export default function AshitaConfig() {
             </Editable>
           </Flex>
           <Flex direction='row' align='center'>
-            <Text as='h2' style={{textTransform: 'capitalize'}}>Game Module</Text>
+            <Text as='h2' style={{textTransform: 'capitalize'}}>Game&nbsp;Module</Text>
             <Editable defaultValue={profile.settings.bootGameModule}
               onSubmit={(next) => dispatch(setSettingsValue({field: 'bootGameModule', value: next}))}
               width='100%'>
@@ -82,6 +82,7 @@ export default function AshitaConfig() {
             </Flex>
         </Editable>
       </Flex>
+      <Text fontSize='12pt' fontStyle='oblique' marginTop='1em'>Launcher Settings</Text>
       <Divider marginTop='5px' marginBottom='5px'/>
       <Flex direction='row' align='center' justifyContent='space-between'>
         <Text as='h2' style={{textTransform: 'capitalize'}}>Automatically&nbsp;Close&nbsp;Launcher</Text>
@@ -89,14 +90,96 @@ export default function AshitaConfig() {
           isChecked={profile.settings.autocloseLauncher}
           onChange={() => {dispatch(setSettingsValue({field: 'autocloseLauncher', value: !profile.settings.autocloseLauncher}))}} />
       </Flex>
+      <Text fontSize='12pt' fontStyle='oblique' marginTop='1em'>Game Settings</Text>
       <Divider marginTop='5px' marginBottom='5px'/>
       <Flex direction='row' align='center' justifyContent='space-between'>
-        <Select 
+        <Select
             placeholder="Language"
             onChange={(e) => {dispatch(setSettingsValue({field:'language', value:parseInt(e.target.value, 10)}))}}>
           <option value={1}>Japanese</option>
           <option value={2}>English</option>
         </Select>
+      </Flex>
+      <Text fontSize='12pt' fontStyle='oblique' marginTop='1em'>Input Settings</Text>
+      <Divider marginTop='5px' marginBottom='5px'/>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Allow&nbsp;gamepad&nbsp;input&nbsp;when&nbsp;not&nbsp;focused</Text>
+        <Switch
+          isChecked={profile.settings.gamepadBackground}
+          onChange={() => {dispatch(setSettingsValue({field: 'gamepadBackground', value: !profile.settings.gamepadBackground}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Disable&nbsp;gamepad&nbsp;enumeration</Text>
+        <Switch
+          isChecked={profile.settings.gamepadDisableEnumeration}
+          onChange={() => {dispatch(setSettingsValue({field: 'gamepadDisableEnumeration', value: !profile.settings.gamepadDisableEnumeration}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Block&nbsp;keyboard&nbsp;input</Text>
+        <Switch
+          isChecked={profile.settings.blockKeyboardInput}
+          onChange={() => {dispatch(setSettingsValue({field: 'blockKeyboardInput', value: !profile.settings.blockKeyboardInput}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Block&nbsp;keybinds&nbsp;during&nbsp;input</Text>
+        <Switch
+          isChecked={profile.settings.gamepadBackground}
+          onChange={() => {dispatch(setSettingsValue({field: 'gamepadBackground', value: !profile.settings.gamepadBackground}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Silent&nbsp;keybinds</Text>
+        <Switch
+          isChecked={profile.settings.silentKeybinds}
+          onChange={() => {dispatch(setSettingsValue({field: 'silentKeybinds', value: !profile.settings.silentKeybinds}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Allow&nbsp;Windows&nbsp;key</Text>
+        <Switch
+          isChecked={profile.settings.windowsKeyEnabled}
+          onChange={() => {dispatch(setSettingsValue({field: 'windowsKeyEnabled', value: !profile.settings.windowsKeyEnabled}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Block&nbsp;mouse&nbsp;input</Text>
+        <Switch
+          isChecked={profile.settings.blockMouseInput}
+          onChange={() => {dispatch(setSettingsValue({field: 'blockMouseInput', value: !profile.settings.blockMouseInput}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Unhook&nbsp;mouse</Text>
+        <Switch
+          isChecked={profile.settings.unhookMouse}
+          onChange={() => {dispatch(setSettingsValue({field: 'unhookMouse', value: !profile.settings.unhookMouse}))}}
+        />
+      </Flex>
+      <Text fontSize='12pt' fontStyle='oblique' marginTop='1em'>In-Game settings</Text>
+      <Divider marginTop='5px' marginBottom='5px'/>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Silent&nbsp;addons</Text>
+        <Switch
+          isChecked={profile.settings.silentAddons}
+          onChange={() => {dispatch(setSettingsValue({field: 'silentAddons', value: !profile.settings.silentAddons}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Silent&nbsp;aliases</Text>
+        <Switch
+          isChecked={profile.settings.silentAliases}
+          onChange={() => {dispatch(setSettingsValue({field: 'silentAliases', value: !profile.settings.silentAliases}))}}
+        />
+      </Flex>
+      <Flex direction='row' align='center' justify='space-between'>
+        <Text>Silent&nbsp;plugins</Text>
+        <Switch
+          isChecked={profile.settings.silentPlugins}
+          onChange={() => {dispatch(setSettingsValue({field: 'silentPlugins', value: !profile.settings.silentPlugins}))}}
+        />
       </Flex>
     </AccordionPanel>
   </AccordionItem>
