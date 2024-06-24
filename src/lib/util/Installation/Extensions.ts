b865036c179b71c46a044eb68954addf8d793f2b
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-24 16:30:18 -0700

    add listing support for POLPlugins

--- src/lib/util/Installation/Extensions.ts
+++ src/lib/util/Installation/Extensions.ts
@@ -1,6 +1,6 @@
 import { readdir } from "fs/promises"
 import { existsSync } from "fs"
-import { ADDON_LOCATION, PLUGIN_LOCATION } from "./paths"
+import { ADDON_LOCATION, PLUGIN_LOCATION, POL_PLUGIN_LOCATION } from "./paths"
 
 export async function getAddonList():Promise<string[]> {
   return (await readdir(ADDON_LOCATION, {withFileTypes: true}))
@@ -12,4 +12,9 @@ export async function getPluginList():Promise<string[]> {
   return (await readdir(PLUGIN_LOCATION, {withFileTypes: true}))
     .filter(entry => entry.isFile() && entry.name.endsWith('.dll'))
     .map(entry => entry.name.replaceAll('.dll','').toLowerCase())
+}
+export async function getPolPluginList(): Promise<string[]> {
+  return (await readdir(POL_PLUGIN_LOCATION, {withFileTypes: true}))
+    .filter(entry => entry.isFile() && entry.name.endsWith('.dll'))
+    .map(entry => entry.name.replaceAll('.dll', '').toLocaleLowerCase())
 }
