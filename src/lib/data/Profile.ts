0a299d9bab3b7a273150d3d63c075822804a5821
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-24 19:56:48 -0700

    add playonline plugins to profile

--- src/lib/data/Profile.ts
+++ src/lib/data/Profile.ts
@@ -6,15 +6,18 @@ export default class Profile {
   public enabledAddons: string[]
 
   public enabledPlugins: string[]
-  
+
+  public enabledPolPlugins: string[]
+
   public settings: AshitaSettings
-  
+
   public serverType: ('retail'|'private')
 
-  constructor(name: string, enabledAddons:string[] = [], enabledPlugins:string[] = [], isPrivate: boolean = false) {
+  constructor(name: string, enabledAddons:string[] = [], enabledPlugins:string[] = [], enabledPolPlugins:string[], isPrivate: boolean = false) {
     this.name = name
     this.enabledAddons = enabledAddons
     this.enabledPlugins = enabledPlugins
+    this.enabledPolPlugins = enabledPolPlugins
     this.settings = new AshitaSettings(name)
     this.serverType = isPrivate? 'private' : 'retail'
   }
