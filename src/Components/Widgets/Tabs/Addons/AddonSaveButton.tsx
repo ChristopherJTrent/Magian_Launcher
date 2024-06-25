ed5e05d29eea6386bbdc56df15ae3bfc82f58cf2
Christopher Trent <ChristopherJTrent@outlook.com>
2024-06-25 11:03:50 -0700

    fix addon save button

--- src/Components/Widgets/Tabs/Addons/AddonSaveButton.tsx
+++ src/Components/Widgets/Tabs/Addons/AddonSaveButton.tsx
@@ -1,15 +1,14 @@
 import { Button, Flex } from "@chakra-ui/react"
 import { CheckIcon } from "@chakra-ui/icons"
-import { createSelector } from "@reduxjs/toolkit"
 import { useAppSelector } from "../../../../lib/store/store"
+import { currentProfile } from "../../../../lib/store/ProfileReducer"
 
 export default function AddonSaveButton() {
-  const profile = useAppSelector(createSelector(state => state.profiles, profiles => profiles.list[profiles.currentProfile]))
-  const settings = useAppSelector(state => state.ashitaSettings)
+  const profile = useAppSelector(currentProfile)
   return <Button marginTop='3px'
   width='100%' onClick={() => {
     window.electron.ipcRenderer.saveScript(profile)
-    window.electron.ipcRenderer.saveProfile(profile, settings)
+    window.electron.ipcRenderer.saveProfile(profile)
   }}>
 <Flex>
   <h1>Save</h1>
